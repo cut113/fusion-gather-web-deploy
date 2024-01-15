f0f075708537a11964e8d232242a25979c96e08e
Huy <97140326+cut113@users.noreply.github.com>
2024-01-15 14:27:43 +0700

    Deploy

--- web/src/auth.config.ts
+++ web/src/auth.config.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
 export default {
+    trustHost: true,
     pages: {
         signIn: "/login",
         signOut: '/'
